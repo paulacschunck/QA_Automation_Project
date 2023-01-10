f479722e998712bf6b5b0c05d5f5e6c582043531
Paula Schunck <paulac.schunck@gmail.com>
2023-01-10 23:33:59 +0000

    Add more files
    Some exercises about JS

---
+++ myProjects/challenges/Challenges Part 2/Challenge3/script.js
@@ -0,0 +1,31 @@
+/*const markMass = 78; // test1
+const johnMass = 92;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+const markImc= (markMass/markHeight ** 2);
+const johnImc = (johnMass/johnHeight ** 2);
+console.log(markImc, johnImc);
+const markHigherImc = markImc > johnImc;
+console.log(markHigherImc);*/
+
+const markMass = 78; // test 2
+const johnMass = 92;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+
+const markImc= (markMass/markHeight ** 2);
+const johnImc = (johnMass/johnHeight ** 2);
+const markHigherImc = markImc > johnImc;
+
+/*console.log(markImc, johnImc);
+const markHigherImc = markImc > johnImc;
+console.log(markHigherImc);*/
+
+if (markImc > johnImc) {
+    console.log(`O IMC do Mark é ${markImc}, maior que o de John, que é ${johnImc}.`)
+} else {
+    console.log(`O IMC de John é ${johnImc}, maior que o de Mark, que é ${markImc}.`)
+}
+
+// 
