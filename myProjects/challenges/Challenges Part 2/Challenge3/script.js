8205ebad198404dde94123f294c11fc303cf2765
Paula Schunck <paulac.schunck@gmail.com>
2023-01-11 23:05:54 +0000

    add exercise
    doing again this challenge

--- myProjects/challenges/Challenges Part 2/Challenge3/script.js
+++ myProjects/challenges/Challenges Part 2/Challenge3/script.js
@@ -7,7 +7,7 @@ const markImc= (markMass/markHeight ** 2);
 const johnImc = (johnMass/johnHeight ** 2);
 console.log(markImc, johnImc);
 const markHigherImc = markImc > johnImc;
-console.log(markHigherImc);*/
+console.log(markHigherImc);
 
 const markMass = 78; // test 2
 const johnMass = 92;
@@ -20,7 +20,7 @@ const markHigherImc = markImc > johnImc;
 
 /*console.log(markImc, johnImc);
 const markHigherImc = markImc > johnImc;
-console.log(markHigherImc);*/
+console.log(markHigherImc);
 
 if (markImc > johnImc) {
     console.log(`O IMC do Mark é ${markImc}, maior que o de John, que é ${johnImc}.`)
@@ -28,4 +28,9 @@ if (markImc > johnImc) {
     console.log(`O IMC de John é ${johnImc}, maior que o de Mark, que é ${markImc}.`)
 }
 
-// 
+*/
+//Refazendo
+
+
+
+
