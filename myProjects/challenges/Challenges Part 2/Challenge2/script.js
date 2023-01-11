a9f4097885ef974f7319f670694f95772ca97aca
Paula Schunck <paulac.schunck@gmail.com>
2023-01-11 23:16:32 +0000

    add exercise
    Doing again this exercise

--- myProjects/challenges/Challenges Part 2/Challenge2/script.js
+++ myProjects/challenges/Challenges Part 2/Challenge2/script.js
@@ -1,5 +1,5 @@
 //Refazendo com mais detalhes
-
+/*
 
 let billValue= prompt('Qual o valor de sua conta? ');
 
@@ -9,3 +9,8 @@ return bill >= 50 && bill <= 300 ? bill * 0.15: bill * 0.20;
 
 const gorjeta =(calcTip(billValue));
 console.log(`Para o valor de ${billValue} de conta, a gorjeta é de : ${gorjeta}`); 
+
+*/
+
+
+
